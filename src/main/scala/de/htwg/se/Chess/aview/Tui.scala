cd4a5e956c19776bed311efbfd3a0c0e09cd3796
Tom Georgi <tom.georgi@htwg-konstanz.de>
2018-05-23 15:42:50 +0200

    Modified TUI. The column is going now from A to H. The row is going now from 8 to 1.

--- src/main/scala/de/htwg/se/Chess/aview/Tui.scala
+++ src/main/scala/de/htwg/se/Chess/aview/Tui.scala
@@ -8,9 +8,23 @@ class Tui {
       case "g" => grid
       case "n" => new Grid(8)
       case _ => input.toList.filter(c => c != ' ').map(c => c.toString) match {
-          case row :: column :: value :: Nil => grid.set(row.toInt - 1, column.toInt - 1, value)
+          case column :: row :: value :: Nil => grid.set(8 - row.toInt, charToValue(column) - 1, value)
           case _ => grid
         }
+    }
+  }
+
+  def charToValue(col: String): Int = {
+    col match {
+      case "A" => 1
+      case "B" => 2
+      case "C" => 3
+      case "D" => 4
+      case "E" => 5
+      case "F" => 6
+      case "G" => 7
+      case "H" => 8
+
     }
   }
 }
