16db7982dd4b9101ae1ad713ddc81d709e6169e5
Tom Georgi <tom.georgi@htwg-konstanz.de>
2018-06-05 16:06:43 +0200

    Created SetCommand, TurnCommand, GameStatus, Command and UndoManager Modified Controller, Grid, and TUI. Modified TUI Outputs. Now The Controller is calling TurnCommand and SetCommand. Deleted method "turn".

--- src/main/scala/de/htwg/se/Chess/aview/Tui.scala
+++ src/main/scala/de/htwg/se/Chess/aview/Tui.scala
@@ -1,7 +1,8 @@
 package de.htwg.se.Chess.aview
 
 import de.htwg.se.Chess.controller.Controller
-import de.htwg.se.Chess.model._
+import de.htwg.se.Chess.model.GameStatus
+import de.htwg.se.Chess.model.GameStatus._
 import de.htwg.se.Chess.util.Observer
 
 class Tui(controller: Controller) extends Observer {
@@ -10,21 +11,33 @@ class Tui(controller: Controller) extends Observer {
 
   val player: (String, String) = ("Player 1", "Player 2")
 
-  println("Eingabeformat: Figur Spalte Reihe")
-  println("Figur: Bauer(B) Turm(T) Läufer(L) Springer(S) König(K) Dame(Q)")
-  println("Spalte: von A bis H")
-  println("Reihe: von 1 bis 8")
+  println("Chess from Tom Georgi and Lukas Rohloff")
+  println("Input format: column - row - new column - new row")
+  println("column: from A to H")
+  println("Row: from 1 to 8")
+  println("For More Information type help")
   update
 
 
   def processInputLine(input: String): Unit = {
     val in = input.split(" ")
     in(0) match {
+      case "help" => println("up -> prints the Grid again\n" +
+        "q -> Leaves the game\n" +
+        "n -> Start a new Game with Player 1 and Player 2\n" +
+        "n - name - name -> Start a new Game with the entered names for player1 and player2\n")
       case "up" => update
       case "q" =>
       case "n" => {
         if (input.length >= 3) controller.createEmptyGrid((in(1), in(2))) else controller.createEmptyGrid(player)
+        println("Input format: column - row - new column - new row\n" +
+          "column: from A to H\n" +
+          "Row: from 1 to 8\n" +
+          "For More Information type help")
+        update
       }
+      case "z" => controller.undo
+      case "y" => controller.redo
       case "set" => trySet(in)
       case _ => {
         processInputMove(in)
@@ -35,7 +48,7 @@ class Tui(controller: Controller) extends Observer {
 
   def trySet(in: Array[String]): Unit = {
     in.toList.filter(c => c != " ").map(c => c.toString) match {
-      case set :: pC :: pR :: value :: color :: Nil => controller.set(charToValue(pC)-1, 8 - pR.toInt, value, color)
+      case _ :: pC :: pR :: value :: color :: Nil => controller.set(charToValue(pC)-1, 8 - pR.toInt, value, color)
     }
   }
 
@@ -64,5 +77,11 @@ class Tui(controller: Controller) extends Observer {
 
   override def update: Unit = {
     println(controller.gridToString)
+    if (controller.gameStatus == NEXT_PLAYER) {
+      println(controller.playerAtTurn.toString + GameStatus.message(controller.gameStatus))
+    } else {
+      println(controller.playerAtTurn.toString + GameStatus.message(controller.gameStatus))
+    }
+
   }
 }
--- src/main/scala/de/htwg/se/Chess/controller/Controller.scala
+++ src/main/scala/de/htwg/se/Chess/controller/Controller.scala
@@ -1,25 +1,22 @@
 package de.htwg.se.Chess.controller
 
+import de.htwg.se.Chess.model.GameStatus._
 import de.htwg.se.Chess.model._
-import de.htwg.se.Chess.util.Observable
+import de.htwg.se.Chess.util.{Observable, UndoManager}
 
 class Controller(var grid: Grid, var player: (Player, Player)) extends Observable {
 
-  def this(grid: Grid, player1: String, player2: String) = this(grid, (Player(player1, Color.WHITE), Player(player2, Color.BLACK)))
+  var gameStatus: GameStatus = IDLE
+  val size: Int = 8
+  private val undoManager = new UndoManager
 
-  def turn(placeCol: Int, placeRow: Int, newPlaceCol: Int, newPlaceRow: Int): Unit = {
-    var whichPlayer: Player = playerAtTurn
-    var result: (Grid, Boolean) = grid.turn(placeCol, placeRow, newPlaceCol, newPlaceRow, whichPlayer, grid)
-    this.grid = result._1
-    notifyObservers
-    if (result._2) setNextPlayer
-  }
+  def this(grid: Grid, player1: String, player2: String) = this(grid, (Player(player1, Color.WHITE), Player(player2, Color.BLACK)))
 
   def playerAtTurn: Player = player._1
   def setNextPlayer: Unit = player = player.swap
 
   def createEmptyGrid(player: (String, String)): Unit = {
-    val grid = new Grid(8).fill()
+    val grid = new Grid(size).fill()
     this.grid = grid
     this.player = (Player(player._1, Color.WHITE), Player(player._2, Color.BLACK))
     notifyObservers
@@ -27,22 +24,23 @@ class Controller(var grid: Grid, var player: (Player, Player)) extends Observabl
 
   def gridToString: String = grid.toString
 
-  def set(col: Int, row: Int, value: String, color: String): Unit = {
-    val c = color match {
-      case "w" => Color.WHITE
-      case "b" => Color.BLACK
-      case "_" => Color.EMPTY
-    }
+  def turn(placeCol: Int, placeRow: Int, newPlaceCol: Int, newPlaceRow: Int): Unit = {
+    undoManager.doStep(new TurnCommand(placeCol, placeRow, newPlaceCol, newPlaceRow, this))
+    notifyObservers
+  }
 
-    val fig : Figure = value match {
-      case "Bauer" | "Pawn" => Pawn(c)
-      case "Turm" | "Rook" => Rook(c)
-      case "Springer" | "Knight" => Knight(c)
-      case "Läufer" | "Bishop" => Bishop(c)
-      case "König" | "King" => King(c)
-      case "Königin" | "Queen" => Queen(c)
-    }
-    this.grid = grid.set(col, row, Some(fig))
+  def set(col: Int, row: Int, value: String, color: String): Unit = {
+    undoManager.doStep(new SetCommand(col, row, value, color, this))
+    notifyObservers
+  }
+
+  def undo: Unit = {
+    undoManager.undoStep
+    notifyObservers
+  }
+
+  def redo: Unit = {
+    undoManager.redoStep
     notifyObservers
   }
 }
