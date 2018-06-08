4900e101b697bb90d4657151b391fe5ecb6dfb59
Lukas <rohloff.lukas@web.de>
2018-06-08 16:54:36 +0200

    Updated Rook

--- src/main/scala/de/htwg/se/Chess/model/Rook.scala
+++ src/main/scala/de/htwg/se/Chess/model/Rook.scala
@@ -1,9 +1,49 @@
 package de.htwg.se.Chess.model
 
-case class Rook_white() {
+case class Rook(c: Color.Value) extends Figure {
 
-}
+  override val color: Color.Value = c
+  override val typ: FigureType.Value = FigureType.Rook
 
-case class Rook_black() {
+  override def move(oldRow: Int, oldCol: Int, newRow: Int, newCol: Int, grid: Grid): Boolean = {
+    val revColor = colorReverse(color)
+    val moves = (-1, 0) :: (1, 0) :: (0, -1) :: (0, 1) :: Nil
+
+    for (i <- moves) {
+      for (j <- 1 to 8) {
+        val move: (Int, Int) = (oldRow + i._1 * j, oldCol + i._2 * j)
+        if (move._1 < 8 && move._2 < 8 && move._1 >= 0 && move._2 >= 0) {
+          if(move == (newRow, newCol)) {
+            if(!wayIsBlocked(oldRow, oldCol), (newRow, newCol), i, grid) {
+              if(grid.cell(move._1, move._2).iSet) {
+                grid.cell(move._1, move._2).value match {
+                  case Some(res) => res.Color match {
+                      case 'color' => return false
+                    case 'revColor' => return true
+                  }
+                  case None => return true
+                }
+              } else return true
+            } else return false
+          }
+        }
+      }
+    }
+    false
+  }
+
+  def wayIsBlocked(odPlace: (Int, Int), newPlace: (Int, Int), direction: (Int, Int), grid): Unit = {
+    val len: Int = oldPlace._1 - newPlace._1
+    for(i <- 1 to len) {
+      val move: (Int, Int) = (oldPlace._1 + direction._1 * i, oldPlace._2 + direction._2 * i)
+      if(grid.cell(move._1, move._2).iSet) return true
+    }
+    false
+  }
+
+  override def colorReverse(color: Color.Value): Color.Value = color match {
+      case Color.WHITE => Color.BLACK
+      case Color.BLACK => Color.WHITE
+  }
 
 }
