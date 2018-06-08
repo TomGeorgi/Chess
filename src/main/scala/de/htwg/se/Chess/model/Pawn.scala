118aa669b1ec2dd3bc5faf03cab4023bd5c52700
Tom Georgi <tom.georgi@htwg-konstanz.de>
2018-06-08 22:01:53 +0200

    Fixed Bug in Bishop and Queen Class. Added Rules for Pawn. Now Pawn can do the Double Jump or the normal Jump. The Pawn can beat diagonal.

--- src/main/scala/de/htwg/se/Chess/model/Pawn.scala
+++ src/main/scala/de/htwg/se/Chess/model/Pawn.scala
@@ -6,8 +6,66 @@ case class Pawn(c: Color.Value) extends Figure {
   override val typ: FigureType.Value = FigureType.PAWN
 
   override def move(oldRow: Int, oldCol: Int, newRow: Int, newCol: Int, grid: Grid): Boolean = {
-    true
+
+    val revColor: Color.Value = colorReverse(color)
+    val blackBeat = (1, 1) :: (1, -1) :: Nil
+    val whiteBeat = (-1, 1) :: (-1, -1) :: Nil
+
+    color match {
+      case Color.WHITE => {
+        if ((oldRow, oldCol) == (6, oldCol)) {
+          if ((oldRow - 2, oldCol) == (newRow, newCol)) {
+            if (grid.cell(newRow + 1, newCol).isSet || grid.cell(newRow, newCol).isSet) return false
+            return true
+          }
+        }
+        if ((oldRow - 1, oldCol) == (newRow, newCol)) {
+          if (grid.cell(newRow, newCol).isSet) return false
+          return true
+        }
+        for (i <- whiteBeat) {
+          if ((oldRow + i._1, oldCol + i._2) == (newRow, newCol)) {
+            if (grid.cell(newRow, newCol).isSet) {
+              grid.cell(newRow, newCol).value match {
+                case Some(res) => res.color match {
+                  case `color` => return false
+                  case `revColor` => return true
+                }
+                case None => return false
+              }
+            }
+          }
+        }
+      }
+      case Color.BLACK => {
+        if ((oldRow, oldCol) == (1, oldCol)) {
+          if ((oldRow + 2, oldCol) == (newRow, newCol)) {
+            if (grid.cell(newRow - 1, newCol).isSet || grid.cell(newRow, newCol).isSet) return false
+            return true
+          }
+        }
+        if ((oldRow + 1, oldCol) == (newRow, newCol)) {
+          if (grid.cell(newRow, newCol).isSet) return false
+          return true
+        }
+        for (i <- blackBeat) {
+          if ((oldRow + i._1, oldCol + i._2) == (newRow, newCol)) {
+            if (grid.cell(newRow, newCol).isSet) {
+              grid.cell(newRow, newCol).value match {
+                case Some(res) => res.color match {
+                  case `color` => return false
+                  case `revColor` => return true
+                }
+                case None => return false
+              }
+            }
+          }
+        }
+      }
+    }
+    false
   }
+
 
   override def colorReverse(color: Color.Value): Color.Value = color match {
     case Color.WHITE => Color.BLACK
--- src/main/scala/de/htwg/se/Chess/model/Queen.scala
+++ src/main/scala/de/htwg/se/Chess/model/Queen.scala
@@ -6,7 +6,41 @@ case class Queen(c: Color.Value) extends Figure {
   override val typ: FigureType.Value = FigureType.QUEEN
 
   override def move(oldRow: Int, oldCol: Int, newRow: Int, newCol: Int, grid: Grid): Boolean = {
-    true
+    val revColor: Color.Value = colorReverse(color)
+    val moves = (-1, -1) :: (-1, 1) :: (1, -1) :: (1, 1) :: (1, 0) :: (-1, 0) :: (0, 1) :: (0, -1) :: Nil
+
+    for (i <- moves) {
+      for (j <- 1 to 8) {
+        val move: (Int, Int) = (oldRow + i._1 * j, oldCol + i._2 * j)
+        if (move._1 < 8 && move._2 < 8 && move._1 >= 0 && move._2 >= 0) {
+          if (move == (newRow, newCol)) {
+            if (!wayIsBlocked((oldRow, oldCol), (newRow, newCol), i, grid)) {
+              if (grid.cell(move._1, move._2).isSet) {
+                grid.cell(move._1, move._2).value match {
+                  case Some(res) => res.color match {
+                    case `color` => return false
+                    case `revColor` => return true
+                  }
+                  case None => return true
+                }
+              } else return true
+            } else return false
+          }
+        }
+      }
+    }
+    false
+  }
+
+  def wayIsBlocked(oldPlace: (Int, Int), newPlace: (Int, Int), direction: (Int, Int), grid: Grid): Boolean = {
+    for (i <- 1 to 8) {
+      val move: (Int, Int) = (oldPlace._1 + direction._1 * i, oldPlace._2 + direction._2 * i)
+      if (move._1 < 8 && move._2 < 8 && move._1 >= 0 && move._2 >= 0) {
+        if (move == newPlace) return false
+        if (grid.cell(move._1, move._2).isSet) return true
+      }
+    }
+    false
   }
 
   override def colorReverse(color: Color.Value): Color.Value = color match {
