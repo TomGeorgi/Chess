5aa7534d84806ebc1a8e3ca24434be4f8cd90182
Lukas <rohloff.lukas@web.de>
2018-06-08 17:47:50 +0200

    updated Bishop

--- src/main/scala/de/htwg/se/Chess/model/Bishop.scala
+++ src/main/scala/de/htwg/se/Chess/model/Bishop.scala
@@ -1,66 +1,22 @@
 package de.htwg.se.Chess.model
 
-case class Rook(c: Color.Value) extends Figure {
+package de.htwg.se.Chess.model
 
+case class Bishop(c: Color.Value) extends Figure {
   override val color: Color.Value = c
-  <<<<<<< HEAD
-  override val typ: FigureType.Value = FigureType.Rook
+  override val typ: FigureType.Value = FigureType.BISHOP
+
 
   override def move(oldRow: Int, oldCol: Int, newRow: Int, newCol: Int, grid: Grid): Boolean = {
-    val revColor = colorReverse(color)
-    val moves = (-1, 0) :: (1, 0) :: (0, -1) :: (0, 1) :: Nil
-
-    for (i <- moves) {
-      for (j <- 1 to 8) {
-        val move: (Int, Int) = (oldRow + i._1 * j, oldCol + i._2 * j)
-        if (move._1 < 8 && move._2 < 8 && move._1 >= 0 && move._2 >= 0) {
-          if(move == (newRow, newCol)) {
-            if(!wayIsBlocked(oldRow, oldCol), (newRow, newCol), i, grid) {
-              if(grid.cell(move._1, move._2).iSet) {
-                grid.cell(move._1, move._2).value match {
-                  case Some(res) => res.Color match {
-                    case 'color' => return false
-                    case 'revColor' => return true
-                  }
-                  case None => return true
-                }
-              } else return true
-            }
-          }
-        }
-      }
-      false
-    }
-
-    def wayIsBlocked(odPlace: (Int, Int), newPlace: (Int, Int), direction: (Int, Int), grid): Unit = {
-      val len: Int = oldPlace._1 - newPlace._1
-      for(i <- 1 to len) {
-        val move: (Int, Int) = (oldPlace._1 + direction._1 * i, oldPlace._2 + direction._2 * i)
-        if(grid.cell(move._1, move._2).iSet) return true
-      }
-      false
-    }
-
-    override def colorReverse(color: Color.Value): Color.Value = color match {
-      case Color.WHITE => Color.BLACK
-      case Color.BLACK => Color.WHITE
-    }
-
-    =======
-
-    override val typ: FigureType.Value = FigureType.ROOK
-
-    override def move(oldRow: Int, oldCol: Int, newRow: Int, newCol: Int, grid: Grid): Boolean = {
-      true
-    }
-
-    override def getType(): FigureType.Value = typ
-
-    override def toString: String = {
-      color match {
-        case Color.BLACK => "♜"
-        case Color.WHITE => "♖"
-      }
-    }
-    >>>>>>> Dev
+    true
   }
+
+  override def getType(): FigureType.Value = typ
+
+  override def toString: String = {
+    color match {
+      case Color.BLACK => "♝"
+      case Color.WHITE => "♗"
+    }
+  }
+}
