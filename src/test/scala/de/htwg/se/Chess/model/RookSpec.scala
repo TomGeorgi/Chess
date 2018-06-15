16bb9414da4d775f931f30289443237f9d37e6a3
Lukas <rohloff.lukas@web.de>
2018-06-15 17:05:36 +0200

    Added Tests for Rook

--- src/test/scala/de/htwg/se/Chess/model/RookSpec.scala
+++ src/test/scala/de/htwg/se/Chess/model/RookSpec.scala
@@ -3,50 +3,42 @@ package de.htwg.se.Chess.model
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
-import de.htwg.se.Chess.controller.Controller
-import de.htwg.se.Chess.model.Rook
 
 @RunWith(classOf[JUnitRunner])
 class RookSpec extends WordSpec with Matchers {
+  var grid: Grid = new Grid(8)
 
   "A Rook" when {
     "set to correct move" should {
-      val grid = new Grid(8)
-      var grid = set(0, 0, Some(Rook(Color.BLACK)))
+      grid = grid.set(0, 0, Some(Rook(Color.BLACK)))
+      print(grid)
       "have return true" in {
-        move(0, 0, 1, 0, grid) should be(true)
+        Rook(Color.BLACK).move(0, 0, 1, 0, grid) should be(true)
       }
     }
 
     "set to invalid move" should {
-      val grid = new Grid(8)
-      var grid = set(0, 0, Some(Rook(Color.BLACK)))
-      var grid = set(0, 1, Some(Rook(Color.BLACK)))
+      grid = grid.set(0, 1, Some(Rook(Color.BLACK)))
       "have return false" in {
-        move(0, 0, 0, 3, grid) should be(false)
+        Rook(Color.BLACK).move(0, 0, 0, 3, grid) should be(false)
       }
 
       "set to correct attack" should {
-        val grid = new Grid(8)
-        var grid = set(0, 0, Some(Rook(Color.BLACK)))
-        var grid = set(2, 0, Some(Rook(Color.WHITE)))
+        grid = grid.set(0, 2, Some(Rook(Color.WHITE)))
         "have return true" in {
-          move(0, 0, 2, 0, grid) should be(true)
+          Rook(Color.BLACK).move(0, 0, 2, 0, grid) should be(true)
         }
       }
 
       "set to incorrect move" should {
-        val grid = new Grid(8)
-        var grid = set(0, 0, Some(Rook(Color.BLACK)))
         "have return false" in {
-          move(0, 0, 1, 1, grid) should be(false)
+          Rook(Color.BLACK).move(0, 0, 1, 1, grid) should be(false)
         }
       }
       "Set to stay" should {
-        val grid = new Grid(8)
-        var grid = set(2, 2, Some(Rook(Color.BLACK)))
+        print(grid)
         "have return false" in {
-          move(2, 2, 2, 2, grid) should be(false)
+          Rook(Color.BLACK).move(0, 0, 2, 2, grid) should be(false)
         }
       }
     }
