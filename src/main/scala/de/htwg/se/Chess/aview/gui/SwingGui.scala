7bbbc905a8edc146219bbe1454b7e7be328bac59
Lukas <rohloff.lukas@web.de>
2018-06-23 16:57:17 +0200

    Updated SwingGui

--- src/main/scala/de/htwg/se/Chess/aview/gui/SwingGui.scala
+++ src/main/scala/de/htwg/se/Chess/aview/gui/SwingGui.scala
@@ -3,17 +3,12 @@ package de.htwg.se.Chess.aview.gui
 import java.awt.Toolkit
 
 import scala.swing._
-import scala.swing.Swing.LineBorder
 import scala.swing.event._
 import de.htwg.se.Chess.controller._
+import de.htwg.se.Chess.model.GameStatus._
 import de.htwg.se.Chess.model.GameStatus
-import de.htwg.se.Chess.util.Observer
-import de.htwg.se.Chess.model.Grid
 
-import scala.swing.event.MouseClicked
 import scala.swing.event.MousePressed
-import scala.swing.event.MouseReleased
-import scala.io.Source._
 
 class CellClicked(val row: Int, val col: Int) extends Event
 
@@ -22,13 +17,58 @@ class SwingGui(controller: Controller) extends Frame {
   val player: (String, String) = ("Player 1", "Player 2")
 
   var screenSize = Toolkit.getDefaultToolkit.getScreenSize
-  //var windowSize = ((screenSize.width min screenSize.height) * 0.8).toInt
+  var windowSize = ((screenSize.width min screenSize.height) * 0.8).toInt
   //preferredSize = new Dimension(windowSize, windowSize)
-  var WindowSize = 500
-  preferredSize = new Dimension(1300, 1300)
+  preferredSize = new Dimension(600, 600)
   listenTo(controller)
 
   menuBar = new MenuBar {
+    val menu = new Menu("File") {
+        mnemonic = Key.F
+        visible_=(true)
+
+        contents += new MenuItem(Action("Empty") { controller.createEmptyGrid(player._1, player._2) })
+        contents += new Menu("Set") {
+          contents += new Menu("King") {
+            contents += new MenuItem(Action("Black") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "King", "b")})
+            contents += new MenuItem(Action("White") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "King", "w")})
+          }
+          contents += new Menu("Queen") {
+            contents += new MenuItem(Action("Black") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Queen", "b")})
+            contents += new MenuItem(Action("White") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Queen", "w")})
+          }
+          contents += new Menu("Bishop") {
+            contents += new MenuItem(Action("Black") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Bishop", "b")})
+            contents += new MenuItem(Action("White") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Bishop", "w")})
+          }
+          contents += new Menu("Knight") {
+            contents += new MenuItem(Action("Black") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Knight", "b")})
+            contents += new MenuItem(Action("White") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Knight", "w")})
+          }
+          contents += new Menu("Rook") {
+            contents += new MenuItem(Action("Black") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Rook", "b")})
+            contents += new MenuItem(Action("White") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Rook", "w")})
+          }
+          contents += new Menu("Pawn") {
+            contents += new MenuItem(Action("Black") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Pawn", "b")})
+            contents += new MenuItem(Action("White") { val c: Option[String] = Dialog.showInput(null, "a-h 1-8", initial = "")
+              setcommand(c, "Pawn", "w")})
+          }
+        }
+      }
+
+  contents += menu
     contents += new Menu("Edit") {
       mnemonic = Key.E
       contents += new MenuItem(Action("Undo") { controller.undo })
@@ -37,10 +77,9 @@ class SwingGui(controller: Controller) extends Frame {
     }
   }
 
-  val board = new Board(controller, preferredSize)
+  var board = new Board(controller, preferredSize)
 
   val panel = new FlowPanel() {
-
     contents += new BoxPanel(Orientation.Vertical) {
       listenTo(this.mouse.clicks)
       contents += board
@@ -52,81 +91,113 @@ class SwingGui(controller: Controller) extends Frame {
     }
   }
 
-  val textpanel = new FlowPanel() {
-    val messages = new TextArea(rows = 10, columns = 10)
-    contents += messages
-    messages.editable_=(false)
-
-    val outputTexScrollPane = new ScrollPane(messages)
-    contents += outputTexScrollPane
+  val labelpanel = new FlowPanel() {
+    val label = new Label("Willkommen")
+    label.horizontalAlignment = Alignment.Center
+    contents += label
   }
 
-  val gridpanel = new GridPanel(3, 1) {
-    object eingabe1 extends TextField(columns = 5)
-
-    val label = new Label("Eingabe für Position der Figur, die Sie gerne Bewegen möchten")
-    val label2 = new Label("oder verwenden sie die Maus zum Bewegen der Spieler")
-
-
+  val labelpanel2 = new FlowPanel() {
+    val label = new Label("")
+    label.horizontalAlignment = Alignment.Center
     contents += label
-    contents += label2
-    contents += eingabe1
-
-
-    listenTo(eingabe1)
-    reactions += {
-      case EditDone(`eingabe1`) => {
-        val in = eingabe1.text.split("[ ]+")
-        in(0) match {
-          case "emp" => {
-            if (in.length >= 3) controller.createEmptyGrid((in(1), in(2))) else controller.createEmptyGrid(player)
-          }
-          case "help" => textpanel.messages.append("\n q -> Leaves the game\n n -> Start a new Game with Player 1 and Player 2\n n - name - name -> Start a new Game with the entered names for player1 and player2\n emp -> Start a new Game with an empty Grid\n emp - name - name -> Start a new Game with an empty Grid and the entered names for player1 and player2 \n")
-          case "n" => {
-            if (in.length >= 3) controller.createNewGrid((in(1), in(2))) else controller.createNewGrid(player)
-            textpanel.messages.append("Input format: column - row - new column - new row\n column: from A to H\n Row: from 1 to 8\n For More Information type help \n")
-          }
-          case "set" => processInputMove(in)
-          case _ => processInputMove(in)
-            eingabe1.text_=("".toString)
-        }
-      }
-    }
-
   }
 
 
   reactions += {
     case event: GridSizeChanged =>
     case event: Played => repaint()
+      labelpanel.label.text = controller.playerAtTurn.name + " is at turn"
+      if (controller.gameStatus == NEXT_PLAYER) {
+        labelpanel2.label.text = ""
+      } else {
+        labelpanel2.label.text = "\n" + controller.playerAtTurn.toString + GameStatus.message(controller.gameStatus)
+      }
   }
 
-  val secondpanel = new GridPanel(2, 1) {
-    contents += textpanel
-    contents += gridpanel
-  }
-
-  contents = new BorderPanel {
-    add(panel, BorderPanel.Position.West)
-    add(secondpanel, BorderPanel.Position.East)
-  }
-
-  title = "HTWG Chess"
-  resizable = true
-  visible = true
-
-  def processInputMove(in: Array[String]): Unit = {
-    //noinspection ScalaStyle
-    in.toList.filter(c => c != " ").map(c => c.toString) match {
-      case placeCol :: placeRow :: newPlaceCol:: newPlaceRow :: Nil =>
-        //println(8 - placeRow.toInt, charToValue(placeCol),8 - newPlaceRow.toInt, charToValue(newPlaceCol))
-        controller.turn(8 - placeRow.toInt, charToValue(placeCol),8 - newPlaceRow.toInt, charToValue(newPlaceCol))
-      case _ :: pC :: pR :: value :: color :: Nil => controller.set(8 - pR.toInt, charToValue(pC),  value, color)
-      case _ :: pC :: pR :: Nil => controller.set(8 - pR.toInt, charToValue(pC), "_", "_")
-      case _ =>
+  def mouseClick(x: Int, y: Int, dimension: Dimension): Unit = {
+    val gridSize = controller.grid.size - 1
+    var stepcol = dimension.width / 8
+    var steprow = dimension.height / 8
+    var col = 0
+    var row = dimension.height
+    if(getClicks() == 1) {
+      val coord = getCoord(x, y, row, col, steprow, stepcol)
+      val oldcoord = getOldCoord()
+      setClicks(2)
+      controller.turn(gridSize - oldcoord._1, oldcoord._2, gridSize - coord._1, coord._2)
+    } else {
+      val coord = getCoord(x, y, row, col, steprow, stepcol)
+      controller.grid.cell(gridSize - coord._1, coord._2).value match {
+        case Some(res) => if(res.color == controller.playerAtTurn.color) {
+            setClicks(1)
+            save(coord._1, coord._2)
+          } else {
+            setClicks(2)
+          }
+        case None => setClicks(2)
+      }
     }
   }
 
+
+  def getCoord(xCoord: Int, yCoord: Int, row: Int, col: Int, steprow: Int, stepcol: Int): (Int, Int) = {
+    val lines = controller.grid.size - 1
+    var fieldrow = -1
+    var fieldcol = -1
+    var rowc = row
+    var colc = col
+    for(a <- 0 to lines) {
+      if(colc <= xCoord && xCoord <= colc + stepcol) {
+        fieldcol = a
+      }
+      colc += stepcol
+    }
+    for(a <- 0 to lines) {
+      if(rowc >= yCoord && yCoord >= rowc - steprow) {
+        fieldrow = a
+      }
+      rowc -= steprow
+    }
+    return (fieldrow, fieldcol)
+  }
+
+  var clickscounter = 0
+  def setClicks(x: Int): Unit = {
+    clickscounter += x
+    if(clickscounter > 1) {
+      clickscounter = 0
+    }
+  }
+
+  def getClicks(): Int = {
+    return clickscounter
+  }
+
+  var xold = 0
+  var yold = 0
+  def save(x: Int, y: Int): Unit = {
+    xold = x
+    yold = y
+  }
+
+  def getOldCoord(): (Int, Int) = {
+    return (xold, yold)
+  }
+
+  var x = ""
+  def setcommand(c: Option[String], figuretype: String, color: String): Unit = {
+    c match {
+      case  Some(s) => x = s.trim()
+      case None =>
+    }
+    val in = x.split("[ ]+")
+    if(in.length == 2) {
+      controller.set(8 - in(1).toInt, charToValue(in(0)), figuretype, color)
+    } else {
+      labelpanel2.label.text = "Wrong input for Set"
+    }
+  }
   def charToValue(col: String): Int = {
     col match {
       case "A" | "a" => 0
@@ -140,78 +211,13 @@ class SwingGui(controller: Controller) extends Frame {
     }
   }
 
-  def mouseClick(x: Int, y: Int, dimension: Dimension): Unit = {
-    val gridSize = controller.grid.size - 1
-    //println(x, y, dimension)
-    var fieldcol = 0
-    var fieldrow = 0
-    var stepcol = dimension.width / 8
-    var steprow = dimension.height / 8
-    //println(row, col)
-    var col = 0
-    var row = dimension.height
-    val lines = controller.grid.size - 1
-    val clicks = getClicks()
-    if(clicks == 1) {
-      for(a <- 0 to lines) {
-        if(col <= x && x <= col + stepcol) {
-          fieldcol = a
-        }
-        col += stepcol
-      }
-      for(a <- 0 to lines) {
-        if(row >= y && y >= row - steprow) {
-          fieldrow = a
-        }
-        row -= steprow
-      }
-      val coord = getOldCoord()
-      setClicks(1)
-      println(gridSize - coord._1, gridSize - coord._2, gridSize - fieldrow, gridSize - fieldcol)
-      controller.turn(gridSize - coord._1, gridSize - coord._2, gridSize - fieldrow, gridSize - fieldcol)
-
-    } else {
-      for(a <- 0 to lines) {
-        if(col <= x && x <= col + stepcol) {
-          fieldcol = a
-        }
-        col += stepcol
-      }
-      for(a <- 0 to lines) {
-        if(row >= y && y >= row - steprow) {
-          fieldrow = a
-        }
-        row -= steprow
-      }
-      if(controller.grid.cell(gridSize- fieldrow, gridSize - fieldcol).isSet) {
-        setClicks(1)
-        save(fieldrow, fieldcol)
-      } else {
-       setClicks(3)
-      }
-    }
+  contents = new BorderPanel {
+    add(labelpanel, BorderPanel.Position.North)
+    add(labelpanel2, BorderPanel.Position.Center)
+    add(panel, BorderPanel.Position.South)
   }
 
-  var Clicks = 0
-  def setClicks(x: Int): Unit = {
-    Clicks += x
-    if(Clicks > 1) {
-      Clicks = 0
-    }
-  }
-
-  def getClicks(): Int = {
-    return Clicks
-  }
-
-  var xold = 0
-  var yold = 0
-  def save(x: Int, y: Int): Unit = {
-    xold = x
-    yold = y
-  }
-
-  def getOldCoord(): (Int, Int) = {
-    return (xold, yold)
-  }
+  title = "HTWG Chess"
+  resizable = true
+  visible = true
 }
